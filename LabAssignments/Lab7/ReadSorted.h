372ae117346d5ea2cc31208efdfd54fb77c84c86
DINESH SUNNY <57325284+DineshSunny@users.noreply.github.com>
2025-10-01 00:32:20 +0000

    update

--- LabAssignments/Lab7/ReadSorted.h
+++ LabAssignments/Lab7/ReadSorted.h
@@ -1,38 +1,51 @@
+// Dinesh Seveti
+// CSCI 201: Lab Assignment 7
+// ReadSorted.h
+// Defines a class for verifying sortedness of numbers read from a file.
+
 #ifndef READ_SORTED_H
 #define READ_SORTED_H
+
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-
 using namespace std;
+
 class ReadSorted {
 public:
-int readSortedArray(float arr[],int n,std::ifstream &file)
-{
-char ch[10];
-int i=0;
-file.getline(ch, 100);
-arr[0] = atof(ch); //first item in array
-for(i=1;i<n&&!file.eof();i++)
-{
-file.getline(ch, 100);
-arr[i] = atof(ch);
-if(arr[i]<arr[i-1]&&arr[i]!=0)
-{
-cout<<"File was not sorted"<<endl;
-return -1;
-}
-}
-display(arr,i);
-}
-void display(float arr[], int n)
-{
-cout<<"Array : "<<endl;
-for(int i=0;i<=n;i++)
-{
-cout<<arr[i]<<" ";
-}
-cout<<endl;
-}
+    // Reads numbers from file into arr up to maxSize
+    // Returns: number of items read if sorted, -1 if unsorted, 0 if empty
+    int readSortedArray(ifstream &fin, float arr[], int maxSize) {
+        float value;
+        int count = 0;
+
+        // Read first value
+        if (!(fin >> value)) {
+            return 0; // empty file
+        }
+        arr[count++] = value;
+
+        // Read remaining values
+        while (fin >> value && count < maxSize) {
+            if (value < arr[count - 1]) {
+                return -1; // not sorted
+            }
+            arr[count++] = value;
+        }
+
+        return count; // number of items successfully read
+    }
+
+    // Display array contents
+    void display(float arr[], int n, ostream &out = cout) {
+        if (n == 0) {
+            out << "File is empty!" << endl;
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            out << arr[i] << " ";
+        }
+        out << endl;
+    }
 };
+
 #endif
--- LabAssignments/Lab7/mainReadSorted.cpp
+++ LabAssignments/Lab7/mainReadSorted.cpp
@@ -1,99 +1,47 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-using namespace std;
+// Dinesh Seveti
+// CSCI 201: Lab Assignment 7
+// mainReadSorted.cpp
+// Driver program to test the ReadSorted class.
 
-//to read the array from file
-int readSortedArray(ifstream &inFile, float *array, int N)
-{
-    //read the first number
-    string num;
-    inFile >> num;
+#include "ReadSorted.h"
 
-    //if no records are present
-    if (num == "")
-        return 0;
-
-    //convert it to float and assign it to 0th index
-    float prev = stof(num);
-    int index = 0; //index for array
-    array[index++] = prev;
-
-    //read the subsequent numbers
-    while (inFile >> num)
-    {
-        float curr = stof(num); //current number
-        //if order is non decreasing
-        if (prev <= curr)
-        {
-            //if index goes out of bounds
-            if (index < N)
-            {
-                array[index++] = curr; //assign the element
-            }
-            prev = curr;
-        }
-        //else return -1
-        else
-            return -1;
-    }
-
-    //return the no. of items read
-    return index;
-}
-
-//create your own display function
-void display(float *array, int n)
-{
-    cout << endl;
-    if (n == 0)
-    {
-        cout << "File is empty!";
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << array[i] << " ";
-    }
-    cout << endl;
-}
-
-int main()
-{
+int main() {
     float array1[20], array2[30];
-
-    //input the file name
     string inputFile;
+
     cout << "Enter the name of input file: ";
     cin >> inputFile;
-    //open the file
-    ifstream inFile(inputFile);
 
-    //read the array
-    int N1 = readSortedArray(inFile, array1, 20);
-    if (N1 == -1)
-        cout << "\nError! The file was not sorted!\n";
-    else
-    {
-        display(array1, N1);
+    ifstream fin(inputFile);
+    if (!fin) {
+        cerr << "Error opening file!" << endl;
+        return 1;
     }
 
-    //close the input file
-    inFile.close();
-    //clearing the stream
-    inFile.ignore();
+    ReadSorted rs;
 
-    inFile.open(inputFile);                       //open the file again
-    int N2 = readSortedArray(inFile, array2, 30); //read the array
-    if (N2 == -1)
+    // Test with array of size 20
+    int N1 = rs.readSortedArray(fin, array1, 20);
+    if (N1 == -1) {
         cout << "\nError! The file was not sorted!\n";
-    else
-    {
-        display(array2, N2);
+    } else {
+        cout << "Array 1 contents: ";
+        rs.display(array1, N1);
     }
-    //Note : if input file is larger than 30 the array would
-    //store upto 30 elements only but the comparisons for
-    //checking the sorted file must still be done even though
-    //the array is filled.
-    //while displaying only the first 30 values would be displayed.
+
+    // Reset file stream for second test
+    fin.clear();
+    fin.seekg(0);
+
+    // Test with array of size 30
+    int N2 = rs.readSortedArray(fin, array2, 30);
+    if (N2 == -1) {
+        cout << "\nError! The file was not sorted!\n";
+    } else {
+        cout << "Array 2 contents: ";
+        rs.display(array2, N2);
+    }
+
+    fin.close();
     return 0;
 }
